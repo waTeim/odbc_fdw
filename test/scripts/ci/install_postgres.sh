8c582366a9d448cc675725830e1748984d36ba80
Paul Ramsey <pramsey@cleverelephant.ca>
2018-02-09 13:51:09 -0800

    Put all PgSQL versions into a matrix

--- test/scripts/ci/install_postgres.sh
+++ test/scripts/ci/install_postgres.sh
@@ -24,10 +24,7 @@ apt-get -y autoremove
 # Install PostgreSQL 
 apt-get -y install postgresql-${PGSQL_VERSION} postgresql-server-dev-${PGSQL_VERSION}
 
-# read dir contents
-find /etc/postgresql/
-
-# configure it to accept local connections from postgres
+# Configure it to accept local connections from postgres
 echo -e "# TYPE  DATABASE        USER            ADDRESS                 METHOD \nlocal   all             postgres                                trust\nlocal   all             all                                     trust\nhost    all             all             127.0.0.1/32            trust" > /etc/postgresql/${PGSQL_VERSION}/main/pg_hba.conf
 
 # Restart PostgreSQL 

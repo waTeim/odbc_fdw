f608a21df763cd5b592901e958fdee8cc9058950
Gonzalo SR <gonzalo@gonzalosr.com>
2018-10-18 14:11:06 +0200

    Modify sqlserver fixture to clear database instead of dropping it - retest

--- test/fixtures/sqlserver_fixtures.sql
+++ test/fixtures/sqlserver_fixtures.sql
@@ -1,27 +1,26 @@
 -- This fixture is going to be executed with TSQL
 
--- Drop everything, dropping the database is not an option in Azure
-DECLARE @sql NVARCHAR(max)=''
 begin
+-- Drop constraints and tables, dropping the database and recreating it would reset pricing tier in Azure
+    DECLARE @sql NVARCHAR(max)=''
+
 	SELECT @sql += ' ALTER TABLE ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) +    ' NOCHECK CONSTRAINT all; '
 	FROM   INFORMATION_SCHEMA.TABLES
 	WHERE TABLE_TYPE = 'BASE TABLE'
 
 	Exec Sp_executesql @sql
-end
 
-begin
 	select @sql = ''
 
 	SELECT @sql += ' Drop table ' + QUOTENAME(TABLE_SCHEMA) + '.'+ QUOTENAME(TABLE_NAME) + '; '
 	FROM   INFORMATION_SCHEMA.TABLES
 	WHERE TABLE_TYPE = 'BASE TABLE'
 	Exec Sp_executesql @sql
-end
+
 
 
 -- Create fixture
-create table sqlserver_test_table(id int, name text);
-go
-insert into sqlserver_test_table values (1, 'aaaa');
+    create table sqlserver_test_table(id int, name text);
+    insert into sqlserver_test_table values (1, 'aaaa');
+end
 go

771df95dee9c14a4b84a78e1be713c881c254cc6
Raul Marin <git@rmr.ninja>
2018-12-04 10:24:57 +0100

    Test with PG11

--- test/scripts/ci/install_postgres.sh
+++ test/scripts/ci/install_postgres.sh
@@ -11,21 +11,17 @@ sudo apt-key adv --keyserver keys.gnupg.net --recv-keys ACCC4CF8
 add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ trusty-pgdg main"
 apt-get update
 
-# Remove those all PgSQL versions except the one we're testing
-PGSQL_VERSIONS=(9.2 9.3 9.4 9.5 9.6 10 11)
-/etc/init.d/postgresql stop # stop travis default instance
-for V in "${PGSQL_VERSIONS[@]}"; do
-    if [ "$V" != "$PGSQL_VERSION" ]; then
-        apt-get -y remove --purge postgresql-${V}
-    fi
-done
-apt-get -y autoremove
+# Remove those all PgSQL versions
+service postgresql stop;
+apt-get remove postgresql* -y
 
-# Install PostgreSQL
-apt-get -y install postgresql-${PGSQL_VERSION} postgresql-server-dev-${PGSQL_VERSION}
+# Install the Postgresql release that we need
+apt-get install -y --allow-unauthenticated --no-install-recommends --no-install-suggests postgresql-$POSTGRESQL_VERSION postgresql-client-$POSTGRESQL_VERSION postgresql-server-dev-$POSTGRESQL_VERSION postgresql-common
 
-# Configure it to accept local connections from postgres
-echo -e "# TYPE  DATABASE        USER            ADDRESS                 METHOD \nlocal   all             postgres                                trust\nlocal   all             all                                     trust\nhost    all             all             127.0.0.1/32            trust" > /etc/postgresql/${PGSQL_VERSION}/main/pg_hba.conf
+# Recreate the cluster with the config we need
+pg_dropcluster --stop $POSTGRESQL_VERSION main
+rm -rf /etc/postgresql/$POSTGRESQL_VERSION /var/lib/postgresql/$POSTGRESQL_VERSION
+pg_createcluster -u postgres --locale C $POSTGRESQL_VERSION main -- -A trust
 
-# Restart PostgreSQL 
-/etc/init.d/postgresql restart ${PGSQL_VERSION}
+# Start the service
+/etc/init.d/postgresql start $POSTGRESQL_VERSION || sudo journalctl -xe
